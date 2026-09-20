ec1eb5669566c5de53821d1d0cd82aba76ec76eb
Vortex | X Software <kevinisraelcollantes@gmail.com>
2026-09-20 15:18:11 -0600

    Actualizar script.lua

--- private/script.lua
+++ private/script.lua
@@ -33,6 +33,7 @@ local games = {
         [142823291]       = "MM2.lua",
         [125927821145949] = "MOUNTAIN.lua",
         [107778070777162] = "StealAnEgg.lua",
+        [189707] = "SurvDisaster.lua",
 }
 
 local function tween(obj, t, props, style, dir)
